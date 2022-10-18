56d080b4f223eea1d8463d5a4a97ac2b94181371
Kaan Koyuncu <koyuncukaan@gmail.com>
2022-10-18 14:44:07 +0300

    update: remove quotes around key mappings

--- lua/shepherd/maps.lua
+++ lua/shepherd/maps.lua
@@ -16,6 +16,10 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
+--Delete quotes around
+keymap.set('n', 'q\"', 'di\"hPl2x')
+keymap.set('n', 'q\'', 'di\'hPl2x')
+
 -- New tab
 keymap.set('n', 'te', ':tabedit')
 -- Split window
