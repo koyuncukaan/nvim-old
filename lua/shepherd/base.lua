89f07de181bd46389a6dbc81b7983d8dd8b4ce8d
Kaan Koyuncu <koyuncukaan@gmail.com>
2023-05-19 20:33:21 +0300

    add blinking cursor

--- lua/shepherd/base.lua
+++ lua/shepherd/base.lua
@@ -41,3 +41,7 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 
 -- Add asterisks in block comments
 vim.opt.formatoptions:append { 'r' }
+
+-- Set cursor to blinking block
+vim.o.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
+
